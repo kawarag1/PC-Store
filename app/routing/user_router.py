0cac7a70a63a9e00ba7e868b3c6f81552ea5dfb0
kawaragi <dima.kretinin.05@mail.ru>
2025-01-08 17:51:37 +0700

    add new endpoint for get request to get users  profile by his login

--- app/routing/user_router.py
+++ app/routing/user_router.py
@@ -6,6 +6,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from app.services.user_service import UserService
+from app.services.user_profile import UserProfile
 
 
 router = APIRouter(
@@ -15,4 +16,10 @@ router = APIRouter(
 @router.post("/registration")
 async def reg(request: UserRequest, session: Session = Depends(get_session)):
     result = await UserService(session).register(request)
+    return result
+
+
+@router.get("/profile/{login}")
+async def profile(login:str, session: Session = Depends(get_session)):
+    result = await UserProfile(session).get_profile(login)
     return result

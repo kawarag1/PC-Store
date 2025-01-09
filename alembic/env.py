c0089ea2badf0b7e090811bd5a4889ce7b465acc
kawarag1 <dima.kretinin.05@mail.ru>
2025-01-09 23:32:16 +0300

    changed configuration to get connection link from .env

--- alembic/env.py
+++ alembic/env.py
@@ -3,12 +3,14 @@ from logging.config import fileConfig
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 from app.models.models import Base
+from app.settings.settings import settings
 
 from alembic import context
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option("sqlalchemy.url", f"{settings.db_url}?async_fallback=True")
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.

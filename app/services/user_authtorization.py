da74a12354ee6b91e515cb94773d23b9940a49d0
kawarag1 <dima.kretinin.05@mail.ru>
2025-01-13 04:33:51 +0300

    changed method, now its get entered password and compares it with hashed password from database

--- app/services/user_authtorization.py
+++ app/services/user_authtorization.py
@@ -1,6 +1,8 @@
 from app.models.models import User
 from app.schemas.requests.user_schema import UserAuth
 from app.database.connector import *
+from app.security.hasher import verify_password
+
 from sqlalchemy import select
 from fastapi import HTTPException
 
@@ -13,12 +15,15 @@ class UserAuthtorization():
     async def authtorization(self, login:str, password:str):
         query = select(User).filter(User.login == login)
         
+        
         result = self.session.execute(query)
         user:UserAuth = result.scalars().first()
+        
         if user is None:
             raise HTTPException(404, "User is not found")
         else:
-            if user.password != password:
+            verify = verify_password(password, user.password)
+            if verify == False:
                 raise HTTPException(404, "Wrong password")
             
             else:

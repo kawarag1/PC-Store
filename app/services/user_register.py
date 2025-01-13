c495e10c4fd92cbc9f796c51dc8d7de351520d6d
kawarag1 <dima.kretinin.05@mail.ru>
2025-01-13 04:32:10 +0300

    changed method, now its sending to database hashed users password

--- app/services/user_register.py
+++ app/services/user_register.py
@@ -1,6 +1,7 @@
 from app.models.models import User
 from app.schemas.requests.user_schema import UserRequest
 from app.database.connector import *
+from app.security.hasher import hash_password
 
 from sqlalchemy import insert
 
@@ -9,11 +10,12 @@ class UserRegister:
         self.session = session
 
     async def register(self, request: UserRequest):
+        password = hash_password(request.password)
         query = (
             insert(User)
             .values(
                 login=request.login,
-                password=request.password,
+                password=password,
                 email=request.email,
                 name=request.name,
                 surname=request.surname,

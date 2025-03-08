611870ecec9db0b24005aaabf10e745c385126e3
kawarag1 <dima.kretinin.05@mail.ru>
2025-03-08 10:56:27 +0300

    added request for fast creating orders

--- app/routing/order_router.py
+++ app/routing/order_router.py
@@ -1,6 +1,7 @@
 from app.database.connector import get_session
 from app.models.models import User
 from app.schemas.request.order_schema import Order as OrderClass
+from app.schemas.request.product import Product as ProductRequest
 from app.security.jwtmanager import get_current_user
 from app.services.order_service import OrderService
 
@@ -20,7 +21,7 @@ async def check(session: Session = Depends(get_session), user: User = Depends(ge
     return result
 
 
-@router.post("/create_order")
-async def create_order(order: OrderClass, session: Session = Depends(get_session), user: User = Depends(get_current_user)):
-    result = await OrderService(session).create_order(user.id, order)
+@router.post("/create_fast_order")
+async def create_order(order: ProductRequest, session: Session = Depends(get_session), user: User = Depends(get_current_user)):
+    result = await OrderService(session).create_fast_order(order, user.id)
     return result
--- app/services/order_service.py
+++ app/services/order_service.py
@@ -1,10 +1,12 @@
-from app.models.models import Order, User
+from app.models.models import Basket, Order, User
 from app.database.connector import *
 
 from fastapi import Depends, HTTPException
 from sqlalchemy import select
 
+from app.services.basket_service import BasketService
 from app.schemas.request.order_schema import Order as OrderClass
+from app.schemas.request.product import Product as ProductRequest
 from app.security.jwtmanager import get_current_user
 
 class OrderService():
@@ -25,22 +27,93 @@ class OrderService():
         return orders
     
 
-    async def create_order(self, order: OrderClass, user_id: int):
-        query = insert(Order).values(
-            user_id = user_id,
-            products_id = order.products_id,
-            cpu_id = order.cpu_id,
-            gpu_id = order.gpu_id,
-            ram_id = order.ram_id,
-            motherboard_id = order.motherboard_id,
-            m2_id = order.m2_id,
-            ssd_id = order.ssd_id,
-            hdd_id = order.hdd_id,
-            case_id = order.case_id,
-            cooler_id = order.cooler_id,
-            pu_id = order.pu_id
-        ).returning(Order)
-
+    async def create_fast_order(self, order: ProductRequest, user_id: int):
+        if "CPU" in order.article:
+            query = insert(Order).values(
+                user_id = user_id,
+                cpu_id = order.id,
+                category_id = 1
+            ).returning(Order)
+        elif "GPU" in order.article:
+            query = insert(Order).values(
+                user_id = user_id,
+                cpu_id = order.id,
+                category_id = 1
+            ).returning(Order)
+        elif "RAM" in order.article:
+            query = insert(Order).values(
+                user_id = user_id,
+                cpu_id = order.id,
+                category_id = 1
+            ).returning(Order)
+        elif "CASE" in order.article:
+            query = insert(Order).values(
+                user_id = user_id,
+                cpu_id = order.id,
+                category_id = 1
+            ).returning(Order)
+        elif "M2" in order.article:
+            query = insert(Order).values(
+                user_id = user_id,
+                cpu_id = order.id,
+                category_id = 1
+            ).returning(Order)
+        elif "SSD" in order.article:
+            query = insert(Order).values(
+                user_id = user_id,
+                cpu_id = order.id,
+                category_id = 1
+            ).returning(Order)
+        elif "HDD" in order.article:
+            query = insert(Order).values(
+                user_id = user_id,
+                cpu_id = order.id,
+                category_id = 1
+            ).returning(Order)
+        elif "MB" in order.article:
+            query = insert(Order).values(
+                user_id = user_id,
+                cpu_id = order.id,
+                category_id = 1
+            ).returning(Order)
+        elif "VENT" in order.article:
+            query = insert(Order).values(
+                user_id = user_id,
+                cpu_id = order.id,
+                category_id = 1
+            ).returning(Order)
+        elif "TOWER" in order.article:
+            query = insert(Order).values(
+                user_id = user_id,
+                cpu_id = order.id,
+                category_id = 1
+            ).returning(Order)
+        elif "PU" in order.article:
+            query = insert(Order).values(
+                user_id = user_id,
+                cpu_id = order.id,
+                category_id = 1
+            ).returning(Order)
+        
         result = self.session.execute(query)
         self.session.commit()
         return result.scalars().first()
+
+        # basket = BasketService(self.session).check_basket(user_id)
+
+        # query = insert(Order).values(
+        #     user_id = user_id,
+        #     products_id = order.products_id,
+        #     cpu_id = order.cpu_id,
+        #     gpu_id = order.gpu_id,
+        #     ram_id = order.ram_id,
+        #     motherboard_id = order.motherboard_id,
+        #     m2_id = order.m2_id,
+        #     ssd_id = order.ssd_id,
+        #     hdd_id = order.hdd_id,
+        #     case_id = order.case_id,
+        #     cooler_id = order.cooler_id,
+        #     pu_id = order.pu_id
+        # ).returning(Order)
+
+        

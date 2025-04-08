6a21dcb0d113f4e4887bbb0d43c14eb397957907
kawarag1 <dima.kretinin.05@mail.ru>
2025-04-08 14:57:50 +0700

    added new method for searching with filters

--- app/routing/search_router.py
+++ app/routing/search_router.py
@@ -12,3 +12,8 @@ router = APIRouter(
 async def get_products(session: AsyncSession = Depends(get_session), word: str | None = None, user: User = Depends(get_current_user)):
     result = await SearchService(session).get_all_products()
     return result
+
+@router.get("/get_filter_products")
+async def get_filter_products(filter:str, session: AsyncSession = Depends(get_session), user: User = Depends(get_current_user)):
+    result = await SearchService(session).get_filter_products(filter)
+    return result
--- app/services/search_service.py
+++ app/services/search_service.py
@@ -2,6 +2,7 @@ from app.database.connector import *
 from app.models.models import *
 
 from sqlalchemy import select
+from fastapi import HTTPException
 from collections import defaultdict
 
 
@@ -16,11 +17,46 @@ class SearchService():
         models = [CPU, GPU, RAM, Motherboard, POWER_UNIT, PC_CASE, HDD, SSD, M2_SSD, VENT, Cooler]
 
         for model in models:
-            query = select(model)
             if word:
-                query.where(model.name.ilike(f"%{word}%"))
+                query = select(model).where(model.name.ilike(f"{word}%"))
+                # query.where(model.name.ilike(f"%{word}%"))
+            else:
+                query = select(model)
             
             products = (await self.session.execute(query)).scalars().all()
             grouped[model.__name__].extend(products)
         
         return dict(grouped)
+
+    async def get_filter_products(self, filter:str) -> dict[str, list]:
+        try:
+            if filter == "Процессоры":
+                query = select(CPU)
+            elif filter == "Видеокарты":
+                query = select(GPU)
+            elif filter == "Оперативная память":
+                query = select(RAM)
+            elif filter == "Кулер":
+                query = select(Cooler)
+            elif filter == "Блок питания":
+                query = select(POWER_UNIT)
+            elif filter == "Корпус":
+                query = select(PC_CASE)
+            elif filter == "Жёсткий диск":
+                query = select(HDD)
+            elif filter == "Твердотельный накопитель":
+                query = select(SSD)
+            elif filter == "M2 накопитель":
+                query = select(M2_SSD)
+            elif filter == "Материнская плата":
+                query = select(Motherboard)
+            elif filter == "Вентиляторы":
+                query = select(VENT)
+
+            result = await self.session.execute(query)
+            return result.scalars().all()
+
+        except:
+            raise HTTPException(status_code = 404, detail = "filter not found")
+        
+        

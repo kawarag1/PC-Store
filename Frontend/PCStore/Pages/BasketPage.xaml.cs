6bc9ce24c40eb37ba197cba04f5ac461b52e4944
kawarag1 <dima.kretinin.05@mail.ru>
2025-04-17 02:19:17 +0300

    added front for basket... now idk how it works..... help

--- Frontend/PCStore/Pages/BasketPage.xaml.cs
+++ Frontend/PCStore/Pages/BasketPage.xaml.cs
@@ -6,4 +6,34 @@ public partial class BasketPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void SelectingAllBox_CheckedChanged(object sender, CheckedChangedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void PlusCounter(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void MinusCounter(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_1(object sender, TappedEventArgs e)
+    {
+
+    }
 }

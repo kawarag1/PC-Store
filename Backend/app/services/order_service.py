8f0e955a139e693af052df0d95be6e37f0a61379
kawarag1 <dima.kretinin.05@mail.ru>
2025-04-24 00:31:26 +0300

    hotfix

--- Backend/app/services/order_service.py
+++ Backend/app/services/order_service.py
@@ -10,12 +10,97 @@ from app.schemas.request.product import Product as ProductRequest
 from app.security.jwtmanager import get_current_user
 
 class OrderService():
+    query_options = [
+        defer(Order.cpu_id),
+        defer(Order.gpu_id),
+        defer(Order.products_id),
+        defer(Order.ram_id),
+        defer(Order.motherboard_id),
+        defer(Order.m2_id),
+        defer(Order.ssd_id),
+        defer(Order.hdd_id),
+        defer(Order.case_id),
+        defer(Order.cooler_id),
+        defer(Order.pu_id),
+        defer(Order.vent_id),
+        selectinload(Order.cpus).defer(CPU.cpu_specs_id),
+        selectinload(Order.cpus).defer(CPU.manufacturer_id),
+        selectinload(Order.cpus).selectinload(CPU.specs).selectinload(CPU_SPECS.sockets),
+        selectinload(Order.cpus).selectinload(CPU.manufacturers),
+
+        selectinload(Order.gpus).defer(GPU.gpu_specs_id),
+        selectinload(Order.gpus).defer(GPU.manufacturer_id),
+        selectinload(Order.gpus).selectinload(GPU.specs).defer(GPU_SPECS.video_memory_type).selectinload(GPU_SPECS.GPU_Memory_Types),
+        selectinload(Order.gpus).selectinload(GPU.manufacturers),
+
+        selectinload(Order.rams).defer(RAM.ram_specs_id),
+        selectinload(Order.rams).defer(RAM.manufacturer_id),
+        selectinload(Order.rams).selectinload(RAM.specs).defer(RAM_SPECS.type_id).selectinload(RAM_SPECS.types),
+        selectinload(Order.rams).selectinload(RAM.specs).defer(RAM_SPECS.ram_quantity).selectinload(RAM_SPECS.ram_quantities),
+        selectinload(Order.rams).selectinload(RAM.manufacturers),
+
+        selectinload(Order.motherboards).defer(Motherboard.motherboard_specs_id),
+        selectinload(Order.motherboards).defer(Motherboard.manufacturer_id),
+        selectinload(Order.motherboards).selectinload(Motherboard.specs).defer(Motherboard_SPECS.form).selectinload(Motherboard_SPECS.forms),
+        selectinload(Order.motherboards).selectinload(Motherboard.specs).defer(Motherboard_SPECS.chipset).selectinload(Motherboard_SPECS.chipsets),
+        selectinload(Order.motherboards).selectinload(Motherboard.specs).defer(Motherboard_SPECS.socket).selectinload(Motherboard_SPECS.sockets),
+        selectinload(Order.motherboards).selectinload(Motherboard.specs).defer(Motherboard_SPECS.M2_Slot).selectinload(Motherboard_SPECS.slots),
+        selectinload(Order.motherboards).selectinload(Motherboard.specs).defer(Motherboard_SPECS.RAM_Type_id).selectinload(Motherboard_SPECS.ram_types),
+        selectinload(Order.motherboards).selectinload(Motherboard.manufacturers),
+
+
+        selectinload(Order.puS).defer(POWER_UNIT.manufacturer_id),
+        selectinload(Order.puS).defer(POWER_UNIT.certificate_id),
+        selectinload(Order.puS).selectinload(POWER_UNIT.manufacturers),
+        selectinload(Order.puS).selectinload(POWER_UNIT.certs),
+
+
+        selectinload(Order.cases).defer(PC_CASE.pc_case_specs_id),
+        selectinload(Order.cases).defer(PC_CASE.manufacturer_id),
+        selectinload(Order.cases).selectinload(PC_CASE.specs).defer(PC_CASE_SPECS.vent_size).selectinload(PC_CASE_SPECS.sizes),
+        selectinload(Order.cases).selectinload(PC_CASE.specs).defer(PC_CASE_SPECS.case_type).selectinload(PC_CASE_SPECS.types),
+        selectinload(Order.cases).selectinload(PC_CASE.manufacturers),
+
+
+        selectinload(Order.hddS).defer(HDD.hdd_specs_id),
+        selectinload(Order.hddS).defer(HDD.manufacturer_id),
+        selectinload(Order.hddS).selectinload(HDD.specs).defer(HDD_SPECS.memory_id).selectinload(HDD_SPECS.memories),
+        selectinload(Order.hddS).selectinload(HDD.manufacturers),
+
+
+        selectinload(Order.ssdS).defer(SSD.ssd_specs_id),
+        selectinload(Order.ssdS).defer(SSD.manufacturer_id),
+        selectinload(Order.ssdS).selectinload(SSD.specs).defer(SSD_SPECS.memory_id).selectinload(SSD_SPECS.memories),
+        selectinload(Order.ssdS).selectinload(SSD.manufacturers),
+
+
+        selectinload(Order.m2_ssdS).defer(M2_SSD.m2_ssd_specs_id),
+        selectinload(Order.m2_ssdS).defer(M2_SSD.manufacturer_id),
+        selectinload(Order.m2_ssdS).defer(M2_SSD.size),
+        selectinload(Order.m2_ssdS).selectinload(M2_SSD.specs).defer(M2_SSD_SPECS.memory_id).selectinload(M2_SSD_SPECS.memories),
+        selectinload(Order.m2_ssdS).selectinload(M2_SSD.manufacturers),
+        selectinload(Order.m2_ssdS).selectinload(M2_SSD.m2Size),
+
+
+        selectinload(Order.vents).defer(VENT.specs_id),
+        selectinload(Order.vents).defer(VENT.manufacturer_id),
+        selectinload(Order.vents).selectinload(VENT.specs),
+        selectinload(Order.vents).selectinload(VENT.manufacturers),
+
+
+        selectinload(Order.coolers).defer(Cooler.cooler_specs_id),
+        selectinload(Order.coolers).defer(Cooler.manufacturer_id),
+        selectinload(Order.coolers).selectinload(Cooler.specs).defer(Cooler_Specs.base_material_id).selectinload(Cooler_Specs.base_material),
+        selectinload(Order.coolers).selectinload(Cooler.specs).defer(Cooler_Specs.radiator_material_id).selectinload(Cooler_Specs.radiator_material),
+        selectinload(Order.coolers).selectinload(Cooler.coolers_sockets).selectinload(Cooler_Socket.sockets),
+        selectinload(Order.coolers).selectinload(Cooler.manufacturers)
+    ]
     def __init__(self, session: AsyncSession):
         self.session = session
 
 
     async def check_orders(self, user_id: int):
-        query = select(Order).filter(Order.user_id == user_id)
+        query = select(Order).filter(Order.user_id == user_id).options(*self.query_options)
 
         result = await self.session.execute(query)
         orders = result.scalars().all()
--- Frontend/PCStore/Services/OrderService.cs
+++ Frontend/PCStore/Services/OrderService.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json;
 using PCStore.Schemas;
+using PCStore.Schemas.DTO;
 using PCStore.Schemas.Request;
 using System;
 using System.Collections.Generic;
@@ -13,6 +14,7 @@ namespace PCStore.Services
     class OrderService
     {
         private const string CreateFastOrderUrl = "https://pcstore.space/v1/order/create_fast_order";
+        private const string CheckOrdersUrl = "https://pcstore.space/v1/order/check";
 
 
         public async Task<bool> CreateOrder(List<ProductItemModel> products)
@@ -69,6 +71,188 @@ namespace PCStore.Services
             {
                 await Shell.Current.DisplayAlert("Ошибка", ex.Message, "OK");
                 return false;
+            }
+        }
+
+        public async Task<List<OrderDTO>> CheckOrder()
+        {
+            try
+            {
+                var request = new HttpRequestMessage(HttpMethod.Get, CheckOrdersUrl);
+                var handler = new AuthentificatedHttpClientService(
+                        new UserService(),
+                        new HttpClientHandler());
+
+                var _client = new HttpClient(handler);
+
+                var _response = _client.SendAsync(request, new CancellationToken());
+                var response = await _response;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseJson = await response.Content.ReadAsStringAsync();
+                    var Orders = JsonConvert.DeserializeObject<List<OrderDTO>>(responseJson);
+
+                    return Orders;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                await Shell.Current.DisplayAlert("Ошибка", ex.Message, "OK");
+                return null;
+            }
+        }
+
+        public async Task<List<ProductItemModel>> OrderList(List<OrderDTO> orders)
+        {
+            try
+            {
+                var result = new List<ProductItemModel>();
+                var productDictionary = new Dictionary<string, ProductItemModel>();
+
+                foreach (var basket in orders)
+                {
+                    void ProcessProduct<T>(T product, Func<T, ProductItemModel> createModel) where T : class
+                    {
+                        if (product == null) return;
+
+                        dynamic dynamicProduct = product;
+                        string key = $"{dynamicProduct.Id}_{dynamicProduct.Article}"; 
+
+                        if (productDictionary.TryGetValue(key, out var existingProduct))
+                        {
+                            existingProduct.Counter++;
+                        }
+                        else
+                        {
+                            var newProduct = createModel(product);
+                            productDictionary.Add(key, newProduct);
+                        }
+                    }
+
+                    ProcessProduct(basket.Cpus, p => new ProductItemModel
+                    {
+                        Id = p.Id,
+                        Name = p.Name,
+                        Cost = p.Cost,
+                        ImageUrl = $"https://pcstore.space/{p.Image}.jpg",
+                        Counter = 1,
+                        Article = p.Article
+                    });
+
+                    ProcessProduct(basket.Gpus, p => new ProductItemModel
+                    {
+                        Id = p.Id,
+                        Name = p.Name,
+                        Cost = p.Cost,
+                        ImageUrl = $"https://pcstore.space/{p.Image}.jpg",
+                        Counter = 1,
+                        Article = p.Article
+                    });
+
+                    ProcessProduct(basket.Rams, p => new ProductItemModel
+                    {
+                        Id = p.Id,
+                        Name = p.Name,
+                        Cost = p.Cost,
+                        ImageUrl = $"https://pcstore.space/{p.Image}.jpg",
+                        Counter = 1,
+                        Article = p.Article
+                    });
+
+                    ProcessProduct(basket.Motherboards, p => new ProductItemModel
+                    {
+                        Id = p.Id,
+                        Name = p.Name,
+                        Cost = p.Cost,
+                        ImageUrl = $"https://pcstore.space/{p.Image}.jpg",
+                        Counter = 1,
+                        Article = p.Article
+                    });
+
+                    ProcessProduct(basket.PuS, p => new ProductItemModel
+                    {
+                        Id = p.Id,
+                        Name = p.Name,
+                        Cost = p.Cost,
+                        ImageUrl = $"https://pcstore.space/{p.Image}.jpg",
+                        Counter = 1,
+                        Article = p.Article
+                    });
+
+                    ProcessProduct(basket.Cases, p => new ProductItemModel
+                    {
+                        Id = p.Id,
+                        Name = p.Name,
+                        Cost = p.Cost,
+                        ImageUrl = $"https://pcstore.space/{p.Image}.jpg",
+                        Counter = 1,
+                        Article = p.Article
+                    });
+
+                    ProcessProduct(basket.HDDs, p => new ProductItemModel
+                    {
+                        Id = p.Id,
+                        Name = p.Name,
+                        Cost = p.Cost,
+                        ImageUrl = $"https://pcstore.space/{p.Image}.jpg",
+                        Counter = 1,
+                        Article = p.Article
+                    });
+
+                    ProcessProduct(basket.SSDs, p => new ProductItemModel
+                    {
+                        Id = p.Id,
+                        Name = p.Name,
+                        Cost = p.Cost,
+                        ImageUrl = $"https://pcstore.space/{p.Image}.jpg",
+                        Counter = 1,
+                        Article = p.Article
+                    });
+
+                    ProcessProduct(basket.M2SSds, p => new ProductItemModel
+                    {
+                        Id = p.Id,
+                        Name = p.Name,
+                        Cost = p.Cost,
+                        ImageUrl = $"https://pcstore.space/{p.Image}.jpg",
+                        Counter = 1,
+                        Article = p.Article
+                    });
+
+                    ProcessProduct(basket.Vents, p => new ProductItemModel
+                    {
+                        Id = p.Id,
+                        Name = p.Name,
+                        Cost = p.Cost,
+                        ImageUrl = $"https://pcstore.space/{p.Image}.jpg",
+                        Counter = 1,
+                        Article = p.Article
+                    });
+
+                    ProcessProduct(basket.Coolers, p => new ProductItemModel
+                    {
+                        Id = p.Id,
+                        Name = p.Name,
+                        Cost = p.Cost,
+                        ImageUrl = $"https://pcstore.space/{p.Image}.jpg",
+                        Counter = 1,
+                        Article = p.Article
+                    });
+
+
+                }
+                result.AddRange(productDictionary.Values);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                await Shell.Current.DisplayAlert("Ошибка", ex.Message, "OK");
+                return null;
             }
         }
     }
